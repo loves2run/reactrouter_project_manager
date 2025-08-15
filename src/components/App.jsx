e9c2be9d3366a1e04ab9fe70ca0aec5d2e42c69e
Brandon Johnson <johnsbr99@gmail.com>
2025-08-15 06:15:04 -0600

    Initial Commit

---
+++ src/components/App.jsx
@@ -0,0 +1,7 @@
+
+export default function App() {
+
+  return(
+    <h1>App.jsx is working!</h1>
+  )
+}
---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react'
+import './index.css'
+import App from './components/App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+
+)
+
