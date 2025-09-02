095eb51a403ba577a1755ee027fd9bc3ff2ce1a8
Brandon Johnson <johnsbr99@gmail.com>
2025-09-02 05:32:27 -0600

    Added react-router-dom to project

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,12 @@
 import { createRoot } from 'react-dom/client'
 import { StrictMode } from 'react'
-import {
-  createBrowserRouter,
-  RouterProvider
-} from 'react-router';
+import { createBrowserRouter } from 'react-router';
+import { RouterProvider } from 'react-router-dom';
 import './index.css'
 import App from './components/App.jsx'
 import ErrorPage from './components/mainContent/Error-Page.jsx';
 import Index from './components/mainContent/Index.jsx';
-import ProjectDisplay from '../src/routes/ProjectDisplay.jsx';
+import ProjectDisplay from './components/mainContent/ProjectDisplay.jsx';
 
 const router = createBrowserRouter([
   {
@@ -31,7 +29,6 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
     <RouterProvider router={router} />
   </StrictMode>
 
